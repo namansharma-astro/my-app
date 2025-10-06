0101a6300f6ca98f58c74bfecc62040b7e291394
namansharma-astro <naman.sharma@evontech.com>
2025-10-06 18:08:40 +0530

    Feature/hello world (#1)
    * Add node app
    
    * Read me changes
    
    ---------
    
    Co-authored-by: naman.sharma-astro <naman.sharma-astro@github.com>

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const requestHandler = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
